c6684de0b056b433da9b0c7768ded5b0931a872f
ImFardad <fardadgoodboy@gmail.com>
2025-07-11 23:27:29 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-import { bootstrap } from "@workadventure/scripting-api-extra";
-
 const CORRECT_PASSWORD = "1234"; // رمز صحیح را اینجا تعیین کنید
+
+let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
+    console.log('Player tags: ', WA.player.tags);
 
     // Listen for messages from the password popup
     window.addEventListener('message', (event) => {
@@ -18,11 +19,15 @@ WA.onInit().then(() => {
 
             if (enteredPassword === CORRECT_PASSWORD) {
                 // Password is correct, close the popup and teleport the player
-                WA.ui.closePopup();
+                if (currentPopup) {
+                    currentPopup.close();
+                }
                 WA.player.teleport("spawn_room2"); // "spawn_room2" نامی است که در Tiled برای نقطه مقصد گذاشتیم
             } else {
                 // Password is wrong
-                WA.ui.closePopup();
+                if (currentPopup) {
+                    currentPopup.close();
+                }
                 WA.ui.displayActionMessage({
                     message: "Wrong password!",
                     callback: () => {
@@ -33,23 +38,18 @@ WA.onInit().then(() => {
         }
     });
 
-}).catch(e => console.error(e));
-console.log('Script started successfully');
-
-let currentPopup: any = undefined;
-
-// Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
         currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    });
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onLeave('clock').subscribe(() => {
+        if (currentPopup !== undefined) {
+            currentPopup.close();
+            currentPopup = undefined;
+        }
+    });
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -57,12 +57,5 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
