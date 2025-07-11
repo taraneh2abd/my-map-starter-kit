ff97fccc2d25626512253eef8923572ed59a64e6
ImFardad <fardadgoodboy@gmail.com>
2025-07-11 22:45:26 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,38 @@
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
+import { bootstrap } from "@workadventure/scripting-api-extra";
+
+const CORRECT_PASSWORD = "1234"; // رمز صحیح را اینجا تعیین کنید
+
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+
+    // Listen for messages from the password popup
+    window.addEventListener('message', (event) => {
+        // We only listen for messages of the correct type
+        if (event.data && event.data.type === 'passwordSubmission') {
+            const enteredPassword = event.data.password;
+
+            if (enteredPassword === CORRECT_PASSWORD) {
+                // Password is correct, close the popup and teleport the player
+                WA.ui.closePopup();
+                WA.player.teleport("spawn_room2"); // "spawn_room2" نامی است که در Tiled برای نقطه مقصد گذاشتیم
+            } else {
+                // Password is wrong
+                WA.ui.closePopup();
+                WA.ui.displayActionMessage({
+                    message: "Wrong password!",
+                    callback: () => {
+                      // You can add what happens after the user closes the message
+                    }
+                });
+            }
+        }
+    });
+
+}).catch(e => console.error(e));
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
